b22de059d9d5a90a90d97eb23716a6c5478c9d86
amanxs <amanarora1408@gmail.com>
2024-05-14 17:38:12 +0530

    client side and server side rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,11 +6,16 @@ interface User{
 }
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'});
+  //nextjs will cache this data on its file system istead of fetching it every time from the network
+  //get data every 10sec
+  //this caching is only on the fetch function. Third party libraries like axios will not be cached.
   const users: User[] = await res.json();
 
   return (
-    <><h1> Users </h1><ul>
+    <><h1> Users </h1>
+    <p>{new Date().toLocaleDateString()}</p>
+    <ul>
       {users.map(user => <li key={user.id}>{user.name}</li>)}
     </ul></>   
   )
